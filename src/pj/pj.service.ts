13f8f6df9f454d5748442178a07b3aec1b197e7a
qudwnals <cwjk1008@naver.com>
2024-09-26 16:24:09 +0900

    First Commit

--- src/pj/pj.service.ts
+++ src/pj/pj.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class PjService {
     getWelcome(): string {
-        return 'welcome nextjs!!!';
+        return 'welcome nestjs!!!';
     }
 }
